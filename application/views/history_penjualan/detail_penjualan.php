7da8b06f532d00652ba7e1b8964626524a0d5be0
Ahmad Fariz <ahmadbinifan@gmail.com>
2022-10-29 02:54:40 +0700

    updated Batal Transaksi

--- application/views/history_penjualan/detail_penjualan.php
+++ application/views/history_penjualan/detail_penjualan.php
@@ -1,15 +1,15 @@
 <!-- Begin Page Content -->
 <div class="container-fluid">
 
-<!-- DataTales Example -->
-<div class="card shadow mb-4">
-  <div class="card-header py-3">
-    <h6 class="m-0 font-weight-bold text-primary text-center">KETERANGAN PENJUALAN</h6>
-  </div>
-  <br>
-  <div class="card-body">
-    <div class="table-responsive">
-      <table class="table table-bordered" width="100%" cellspacing="0">
+  <!-- DataTales Example -->
+  <div class="card shadow mb-4">
+    <div class="card-header py-3">
+      <h6 class="m-0 font-weight-bold text-primary text-center">KETERANGAN PENJUALAN</h6>
+    </div>
+    <br>
+    <div class="card-body">
+      <div class="table-responsive">
+        <table class="table table-bordered" width="100%" cellspacing="0">
           <tr>
             <td width="220">No Faktur</td>
             <td width="20">:</td>
@@ -35,20 +35,23 @@
             <td>:</td>
             <td>Rp. <?= number_format($jual['jual_total']); ?></td>
           </tr>
-      </table>
+        </table>
+      </div>
+      <div class="row float-sm-right mr-3">
+        <button class="btn btn-danger" onclick="batal('<?= $jual['jual_nofak']; ?>')">Batal</button>
+      </div>
     </div>
   </div>
-</div>
 
-<!--  -->
-<div class="card shadow mb-4">
-  <div class="card-header py-3">
-    <h6 class="m-0 font-weight-bold text-primary text-center">DAFTAR BARANG</h6>
-  </div>
-  <br>
-  <div class="card-body">
-    <div class="table-responsive">
-      <table class="table table-bordered" width="100%" cellspacing="0">
+  <!--  -->
+  <div class="card shadow mb-4">
+    <div class="card-header py-3">
+      <h6 class="m-0 font-weight-bold text-primary text-center">DAFTAR BARANG</h6>
+    </div>
+    <br>
+    <div class="card-body">
+      <div class="table-responsive">
+        <table class="table table-bordered" width="100%" cellspacing="0">
           <tr>
             <th class="text-center">No</th>
             <th class="text-center">Nama Barang</th>
@@ -57,20 +60,21 @@
             <th class="text-center">Keterangan</th>
             <th class="text-center">Harga Total</th>
           </tr>
-          <?php $no=1; foreach($dt_jual as $a){ ?>
-          <tr>
-            <td class="text-center"><?= $no++; ?></td>
-            <td class="text-center"><?= $a['d_jual_barang_nama']; ?></td>
-            <td class="text-center">Rp. <?= number_format($a['d_jual_barang_harjul']); ?></td>
-            <td class="text-center"><?= $a['d_jual_qty']; ?></td>
-            <td class="text-center"><?= $a['d_jual_diskon']; ?></td>
-            <td class="text-center">Rp. <?= number_format($a['d_jual_total']); ?></td>
-          </tr>
+          <?php $no = 1;
+          foreach ($dt_jual as $a) { ?>
+            <tr>
+              <td class="text-center"><?= $no++; ?></td>
+              <td class="text-center"><?= $a['d_jual_barang_nama']; ?></td>
+              <td class="text-center">Rp. <?= number_format($a['d_jual_barang_harjul']); ?></td>
+              <td class="text-center"><?= $a['d_jual_qty']; ?></td>
+              <td class="text-center"><?= $a['d_jual_diskon']; ?></td>
+              <td class="text-center">Rp. <?= number_format($a['d_jual_total']); ?></td>
+            </tr>
           <?php } ?>
-      </table>
+        </table>
+      </div>
     </div>
   </div>
-</div>
 
 </div>
 <!-- /.container-fluid -->
--- application/views/history_penjualan/index.php
+++ application/views/history_penjualan/index.php
@@ -1,82 +1,92 @@
 <!-- Begin Page Content -->
 <div class="container-fluid">
 
-<!-- Page Heading -->
-<h1 class="h3 mb-2 text-gray-800">Data History Penjualan</h1>
+  <!-- Page Heading -->
+  <h1 class="h3 mb-2 text-gray-800">Data History Penjualan</h1>
 
-<!-- DataTales Example -->
-<div class="card shadow mb-4">
-  <div class="card-header py-3">
-  <!-- FLASH DATA -->    
-  <?php 
+  <!-- DataTales Example -->
+  <div class="card shadow mb-4">
+    <div class="card-header py-3">
+      <!-- FLASH DATA -->
+      <?php
       $dat = $this->session->flashdata('msg');
-          if($dat!=""){ ?>
-                <div id="notifikasi" class="alert alert-success"><strong>Sukses! </strong> <?=$dat;?></div>
-      <?php } ?> 
-    <h6 class="m-0 font-weight-bold text-primary">Daftar History Penjualan</h6>
-  </div>
-  <br>
-  <div class="card-body">
+      if ($dat != "") { ?>
+        <div id="notifikasi" class="alert alert-success"><strong>Sukses! </strong> <?= $dat; ?></div>
+      <?php } ?>
+      <h6 class="m-0 font-weight-bold text-primary">Daftar History Penjualan</h6>
+    </div>
     <br>
-    <div class="table-responsive">
-      <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-        <thead>
-          <tr>
-            <th>No</th>
-            <th>Nama PLG</th>
-            <th>Tanggal</th>
-            <th>No HP</th>
-            <th>No Faktur</th>
-            <th>Jumlah</th>
-            <th>Total Harga</th>
-            <th>Aksi</th>
-          </tr>
-        </thead>
-        <tfoot>
-          <tr>
-            <th>No</th>
-            <th>Nama PLG</th>
-            <th>Tanggal</th>
-            <th>No HP</th>
-            <th>No Faktur</th>
-            <th>Jumlah</th>
-            <th>Total Harga</th>
-            <th>Aksi</th>
-          </tr>
-        </tfoot>
-        <tbody>
-        <?php $no=1; foreach($data as $a){ ?>
-            <?php
-                $no_fak = $a['jual_nofak'];
-                $jmlh = $this->db->query("select count(d_jual_nofak) as jum from tbl_detail_jual where d_jual_nofak='$no_fak'")->row_array();
-            ?>
-
-          <tr>
-            <td><?= $no++; ?></td>
-            <td><?php echo $a['namaplg']; ?></td>
-            <td><?php echo $a['jual_tanggal']; ?></td>
-            <td><?php echo $a['no_hp']; ?></td>
-            <td><?= $a['jual_nofak']; ?></td>
-            <td><?= $jmlh['jum']; ?> items</td>
-            <td>Rp. <?= number_format($a['jual_total']); ?></td>
-            <td class="text-center">
-              <a class="badge badge-success" href="<?= base_url(); ?>history_penjualan/in_detail/<?= $a['jual_nofak']; ?>">View</a>
+    <div class="card-body">
+      <br>
+      <div class="table-responsive">
+        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Nama PLG</th>
+              <th>Tanggal</th>
+              <th>No HP</th>
+              <th>No Faktur</th>
+              <th>Jumlah</th>
+              <th>Total Harga</th>
+              <th>Status</th>
+              <th>Aksi</th>
+            </tr>
+          </thead>
+          <tfoot>
+            <tr>
+              <th>No</th>
+              <th>Nama PLG</th>
+              <th>Tanggal</th>
+              <th>No HP</th>
+              <th>No Faktur</th>
+              <th>Jumlah</th>
+              <th>Total Harga</th>
+              <th>Status</th>
+              <th>Aksi</th>
+            </tr>
+          </tfoot>
+          <tbody>
+            <?php $no = 1;
+            foreach ($data as $a) { ?>
               <?php
-                if($this->session->userdata("level")=="admin"){
-              
+              $no_fak = $a['jual_nofak'];
+              $jmlh = $this->db->query("select count(d_jual_nofak) as jum from tbl_detail_jual where d_jual_nofak='$no_fak'")->row_array();
+              if ($a['status'] == "COMPLETE") {
+                $status = '<div class="badge badge-success">Complete</div>';
+              } else {
+                $status = '<div class="badge badge-danger">Cancel</div>';
+              }
               ?>
-              <a class="badge badge-success" href="<?= base_url(); ?>history_penjualan/cetak_faktur/<?= $a['jual_nofak']; ?>" target="_blank">Cetak</a>
-              <a class="badge badge-success" href="<?= base_url(); ?>penjualan_edit/index/<?= $a['jual_nofak']; ?>">Edit</a>
-              <a class="badge badge-success" href="<?= base_url(); ?>history_penjualan/takdeletekowe/<?= $a['jual_nofak']; ?>">Delete</a>
-              <?php } ?>
-           </td>
-          </tr>
-        <?php } ?>
-        </tbody>
-      </table>
+
+              <tr>
+                <td><?= $no++; ?></td>
+                <td><?php echo $a['namaplg']; ?></td>
+                <td><?php echo $a['jual_tanggal']; ?></td>
+                <td><?php echo $a['no_hp']; ?></td>
+                <td><?= $a['jual_nofak']; ?></td>
+                <td><?= $jmlh['jum']; ?> items</td>
+                <td>Rp. <?= number_format($a['jual_total']); ?></td>
+                <td><?= $status ?></td>
+                <td class="text-center">
+                  <a class="badge badge-success" href="<?= base_url(); ?>history_penjualan/in_detail/<?= $a['jual_nofak']; ?>">View</a>
+                  <?php
+                  if ($this->session->userdata("level") == "admin") {
+
+                  ?>
+                    <a class="badge badge-success" href="<?= base_url(); ?>history_penjualan/cetak_faktur/<?= $a['jual_nofak']; ?>" target="_blank">Cetak</a>
+                    <a class="badge badge-success" href="<?= base_url(); ?>penjualan_edit/index/<?= $a['jual_nofak']; ?>">Edit</a>
+                    <a class="badge badge-success" href="<?= base_url(); ?>history_penjualan/takdeletekowe/<?= $a['jual_nofak']; ?>">Delete</a>
+                    <a class="badge badge-danger" href="<?= base_url(); ?>history_penjualan/batal/<?= $a['jual_nofak']; ?>">Batal</a>
+                  <?php } ?>
+                </td>
+              </tr>
+            <?php } ?>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
-</div>
 
 </div>
 <!-- /.container-fluid -->
